f4146ea9ad9a2fd93e087353ccfd420ab2d76657
Bibek Dhimal <bibekdhimal7@gmail.com>
2022-01-04 10:14:40 +0545

    GolfBall

---
+++ GolfBall.java
@@ -0,0 +1,10 @@
+public class GolfBall implements Playable {
+    
+    public static void main(String[] args) {
+    @Override
+   
+        System.out.println("Playing GolfBall ");
+
+    
+}
+}
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
-class HelloWorld {
+public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+         
+       int s = 35;
+       int areaSquare = s*s;
+       System.out.println("area of square is ="+ area_square);
+       
+ 
     }
-}
-
-
+ }
